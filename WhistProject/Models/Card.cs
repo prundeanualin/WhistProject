9b7cd8e94acc8ae3e495042dbf0f1cef7bda1cc8
filipmarchidan <marchidanfilip@yahoo.com>
2019-07-12 17:20:16 +0200

    add card model

---
+++ WhistProject/Models/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhistProject.Models
+{
+    public class Card
+    {   
+        public int number { get; set; }
+
+        public Card card(int number)
+        {
+            this.number = number;
+            if (checkRange(number) == true)
+            {
+                Card result = card(number);
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        protected Boolean checkRange(int number)
+        {
+            if (this.number <= 15 && this.number > 0)
+                return true;
+            return false;
+        }
+
+    }
+}
