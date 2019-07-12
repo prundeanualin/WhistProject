c255ad30a73da0db8819fbeb2b05b89ef0365a33
filipmarchidan <marchidanfilip@yahoo.com>
2019-07-12 14:51:36 +0200

    Massive design update (html + css) -add new cyborg theme -edit the layout bar -add resources for photos -altered the pages

--- WhistProject/App_Start/BundleConfig.cs
+++ WhistProject/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace WhistProject
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cyborg.css",
                       "~/Content/site.css"));
         }
     }
