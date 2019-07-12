fa9c27f6b90e40530d3dd82b4565e0b3557626b0
filipmarchidan <marchidanfilip@yahoo.com>
2019-07-12 18:31:50 +0200

    add player entity

---
+++ WhistProject/Models/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhistProject.Models
+{
+    public class Player
+    {
+        public String username { get; set; }
+        public int score { get; set; }
+
+        public Player(String username, int score)
+        {
+            this.username = username;
+            this.score = score;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var player = obj as Player;
+            return player != null &&
+                   username == player.username &&
+                   score == player.score;
+        }
+    }
+}
