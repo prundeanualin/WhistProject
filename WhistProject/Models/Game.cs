30e8dbf3857a1d55f4a3e9049170c63f7f5ae0d8
filipmarchidan <marchidanfilip@yahoo.com>
2019-07-14 00:13:19 +0200

    add Hand implementation - algorithm for one hand which goes through the rounds Game implementation - algorithm for all hands which puts them all together in a game

---
+++ WhistProject/Models/Game.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhistProject.Models
+{
+    public class Game
+    {
+        public Hand hand;
+        public int counter;
+        public int numberOfOnes;
+        public int numberOfEights;
+        public int nrOfHandsPlayed;
+        public int expectedToPlay;
+        public List<Player> players;
+        public Game(List<Player> players)
+        {
+            this.players = players;
+        }
+        public int getSize(List<Player> players)
+        {
+            this.players = players;
+            return players.Capacity;
+        }
+        public void startGame()
+        {
+            //pretty straight forward logic of the whist game algorithm
+            // based on hands already implemented
+            // rest of magic happens in class hand
+            numberOfEights = this.players.Capacity;
+            numberOfOnes = this.players.Capacity * 2;
+            expectedToPlay = numberOfEights + numberOfOnes + 12;
+            for(int i=0; i< numberOfOnes/2;i++)
+            {
+                this.hand = new Hand(players, 1);
+                this.hand.playHand();
+                nrOfHandsPlayed++;
+            }
+            for(int i = 2; i<=7; i++)
+            {
+                this.hand = new Hand(players, i);
+                this.hand.playHand();
+                nrOfHandsPlayed++;
+            }
+            for(int i=0; i< numberOfEights; i++)
+            {
+                this.hand = new Hand(players, 8);
+                this.hand.playHand();
+                nrOfHandsPlayed++;
+            }
+            for(int i= 7; i>=2; i--)
+            {
+                this.hand = new Hand(players, i);
+                this.hand.playHand();
+                nrOfHandsPlayed++;
+            }
+            for(int i=0; i<numberOfOnes/2;i++)
+            {
+                this.hand = new Hand(players, 1);
+                this.hand.playHand();
+                nrOfHandsPlayed++;
+            }
+            if (isCompleted())
+                gameCompleted();
+           
+        }
+        public Boolean isCompleted()
+        {
+            if (nrOfHandsPlayed == expectedToPlay)
+                return true;
+            return false;
+        }
+        public void gameCompleted()
+        {
+            /**
+             * this should contain the implementation of the engine which ends the game
+             * and takes you back to lobby
+             * in this method there should also be an update of the users score
+             */
+             
+        }
+        public void updateScores()
+        {
+            for(int i = 0; i< this.players.Capacity; i++)
+            {
+                //for each player(i) update the score + ingame score
+            }
+        }    
+   
+    }
+}
---
+++ WhistProject/Models/Hand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhistProject.Models
+{
+    public class Hand
+    {
+        List<Player> players;
+        public int typeOfHand;
+        public Boolean complete;
+        public int rotateNumber = 0;
+        public int rotateForOneRound = 0;
+        /**
+         * there is need for a method which flushes the deck
+         * and then cards are given to player corresponding the typeOfHand
+         */
+        public Hand(List<Player> players, int typeOfHand)
+        {
+            this.players = players;
+            this.typeOfHand = typeOfHand;
+        }
+        public void doOneRound()
+        {
+            int capacity = this.players.Capacity;
+            for (int i = 0; i< capacity; i++)
+            {
+                //wait for player to drop card (needs to be implemented)
+                // i'm thinking to use the deck class
+                /// rotateforoneround gives player the access to drop card.
+                rotateForOneRound++;
+                if (rotateForOneRound == capacity)
+                    rotateNumber++;
+            }
+            //in this method we should also determine who lost and who won 
+            //as well a method where user proposes how many hands will do
+        }
+        public void playHand()
+        {
+            for(int i =0; i<typeOfHand; i++)
+            {
+                if (this.rotateNumber == this.typeOfHand)
+                    this.complete = true;
+                doOneRound();
+            }
+            //concretization of the playability of a hand
+        }
+        //checks if hand is compeleted.
+        public Boolean isCompleted()
+        {
+            if (this.rotateNumber == this.typeOfHand)
+                return true;
+            return false;
+        }
+            
+    }
+}
